a0cfdf5841f2f7f051a6a048298d0033e552affb
Tuukka Aro <tuukka.t.aro@utu.fi>
2023-11-16 13:47:09 +0200

    sad comment

--- src/app/api/station/route.ts
+++ src/app/api/station/route.ts
@@ -5,7 +5,7 @@ type ResponseData = {
 }
 
 const { pool } = require("../../../db");
-
+//For no this file remains unused, since for whatever reason Next wouldn't stop compiling it as static page file.
 export async function GET(request: NextRequest, context: any) {
 
     try {
