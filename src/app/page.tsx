f73a6e507c95d13ee001902511a5789472ad92aa
Tuukka Aro <tuukka.t.aro@utu.fi>
2023-11-16 12:01:44 +0200

    db host set to pod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,6 @@ import Table from '../components/table'
 import TablePlaceholder from '../components/table-placeholder'
 import ExpandingArrow from '../components/expanding-arrow'
 
-export const runtime = 'edge'
-export const preferredRegion = 'home'
-export const dynamic = 'force-dynamic'
-
 export default function Home() {
     return (
         <main className="relative flex min-h-screen flex-col items-center m-20">
--- src/db.js
+++ src/db.js
@@ -1,13 +1,14 @@
 import { Pool } from 'pg'
 
 export const pool = new Pool({
+    // Move to ENV configuration file
     user: "academy",
     database: "citybike",
     password: "academy",
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
     port: 5432,
-    host: "localhost",
+    host: "dev-academy-spring-2024-exercise-db-1",
 });
   
 module.exports = { pool };
