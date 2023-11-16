27c7ff596e2913c262bda0124df6b98c90c75c1a
Tuukka Aro <tuukka.t.aro@utu.fi>
2023-11-16 10:44:20 +0200

    station data calc

--- src/app/api/station/[id]/route.ts
+++ src/app/api/station/[id]/route.ts
@@ -7,9 +7,28 @@ type ResponseData = {
 const { pool } = require("../../../../db");
 
 export async function GET(request: NextRequest, context: any) {
-    
-    const query = await pool.query(
+
+    const stations = await pool.query(
         'SELECT * FROM station WHERE id = ' + context.params.id,
     );
-    return NextResponse.json(query.rows);
+    const departures = await pool.query(
+        'SELECT * FROM journey WHERE departure_station_id = ' + stations.rows[0].id,
+    );
+    const returns = await pool.query(
+        'SELECT * FROM journey WHERE return_station_id = ' + stations.rows[0].id,
+    );
+    let distanceSum: number = departures.rows.map(a => a.distance).reduce(function (a, b) {
+        return a + b;
+    });
+    let durationSum: number = departures.rows.map(a => a.duration).reduce(function (a, b) {
+        return a + b;
+    });
+    return NextResponse.json({
+        station: stations.rows[0],
+        departuresTotal: departures.rows.length,
+        distanceAverage: distanceSum / departures.rows.length,
+        durationAverage: durationSum / departures.rows.length,
+        returnsTotal: returns.rows.length,
+
+    });
 }
--- src/components/StationView.tsx
+++ src/components/StationView.tsx
@@ -6,15 +6,21 @@ interface Station {
     [key: string]: any
 }
 
+interface StationData {
+    [key: string]: any
+}
+
 export default function StationView(props: Readonly<{ stationId: string }>) {
-    const [stations, setStations] = useState<Station[] | undefined>(undefined);
+    const [station, setStation] = useState<Station | undefined>(undefined);
+    const [data, setData] = useState<StationData | undefined>(undefined);
 
     useEffect(() => {
         fetch('api/station/' + props.stationId)
             .then((res) => res.json())
             .then((json) => {
                 console.log('arvot', json);
-                setStations(json);
+                setStation(json.station);
+                setData(json);
             });
     }, []);
 
@@ -25,19 +31,85 @@ export default function StationView(props: Readonly<{ stationId: string }>) {
                     <h2 className="text-xl font-semibold">Station {props.stationId}</h2>
                 </div>
                 <div className="divide-y divide-gray-900/5">
-                    {stations?.map((station) => (
                         <div
-                            key={station.id}
+                            key={station?.id}
                             className="flex items-center justify-between py-3"
                         >
                             <div className="flex items-center space-x-4">
                                 <div className="space-y-1">
-                                    <p className="font-medium leading-none">{station.station_name}</p>
-                                    <p className="text-sm text-gray-500">{station.station_address}</p>
+                                    <p className="font-medium leading-none">{station?.station_name}</p>
+                                    <p className="text-sm text-gray-500">{station?.station_address}</p>
                                 </div>
                             </div>
                         </div>
-                    ))}
+                </div>
+            </div>
+            <div className="flex justify-between items-center mb-4">
+                <div className="space-y-1">
+                    <h2 className="text-xl font-semibold">Journeys starting from the station</h2>
+                </div>
+                <div className="divide-y divide-gray-900/5">
+                        <div
+                            key={station?.id}
+                            className="flex items-center justify-between py-3"
+                        >
+                            <div className="flex items-center space-x-4">
+                                <div className="space-y-1">
+                                    <p className="text-sm text-gray-500">{data?.departuresTotal}</p>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+            <div className="flex justify-between items-center mb-4">
+                <div className="space-y-1">
+                    <h2 className="text-xl font-semibold">Journeys ending at the station</h2>
+                </div>
+                <div className="divide-y divide-gray-900/5">
+                        <div
+                            key={station?.id}
+                            className="flex items-center justify-between py-3"
+                        >
+                            <div className="flex items-center space-x-4">
+                                <div className="space-y-1">
+                                    <p className="text-sm text-gray-500">{data?.returnsTotal}</p>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+            <div className="flex justify-between items-center mb-4">
+                <div className="space-y-1">
+                    <h2 className="text-xl font-semibold">Average distance of journeys</h2>
+                </div>
+                <div className="divide-y divide-gray-900/5">
+                        <div
+                            key={station?.id}
+                            className="flex items-center justify-between py-3"
+                        >
+                            <div className="flex items-center space-x-4">
+                                <div className="space-y-1">
+                                    <p className="text-sm text-gray-500">{''+Math.floor(data?.distanceAverage) +'m'}</p>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+            <div className="flex justify-between items-center mb-4">
+                <div className="space-y-1">
+                    <h2 className="text-xl font-semibold">Avarage duration of journeys</h2>
+                </div>
+                <div className="divide-y divide-gray-900/5">
+                        <div
+                            key={station?.id}
+                            className="flex items-center justify-between py-3"
+                        >
+                            <div className="flex items-center space-x-4">
+                                <div className="space-y-1">
+                                    <p className="text-sm text-gray-500">{Math.floor(data?.durationAverage) + ' min'}</p>
+                                </div>
+                            </div>
+                        </div>
                 </div>
             </div>
          </div>
